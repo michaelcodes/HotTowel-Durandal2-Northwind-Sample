37d1d2462a31f892ae6831b16686591eea78a752
michaelcodes <imuedon@gmail.com>
2013-08-22 12:37:34 -0500

    EF Code First Models Cleanup and Order Styling

--- HotTowelNorthwind/Models/Northwind.cs
+++ HotTowelNorthwind/Models/Northwind.cs
@@ -11,29 +11,14 @@ namespace HotTowelNorthwind.Models
 {
     public class NorthwindContext : DbContext
     {
-        // Use the constructor to target a specific named connection string
-        public NorthwindContext()
-            : base("name=NorthwindDB")
-        {
-            // Disable proxy creation as this messes up the data service.
-            //this.Configuration.ProxyCreationEnabled = false;
-
-            // Create Northwind if it doesn't already exist.
-            //this.Database.CreateIfNotExists();
-        }
-
-        // Disable creation of the EdmMetadata table.
-        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Conventions.Remove<IncludeMetadataConvention>();
-        //}
+        public NorthwindContext() : base("name=NorthwindDB") {}
 
         public DbSet<Customer> Customers { get; set; }
-        public DbSet<Employee> Employees { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
         public DbSet<Product> Products { get; set; }
     }
+
     [Table("Customers")]
     public class Customer
     {
@@ -50,34 +35,12 @@ namespace HotTowelNorthwind.Models
         public string Fax { get; set; }
         public virtual ICollection<Order> Orders { get; set; }
     }
-    [Table("Employees")]
-    public class Employee
-    {
-        public int EmployeeID { get; set; }
-        public string LastName { get; set; }
-        public string FirstName { get; set; }
-        public string Title { get; set; }
-        public string TitleOfCourtesy { get; set; }
-        public DateTime? BirthDate { get; set; }
-        public DateTime? HireDate { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string Region { get; set; }
-        public string PostalCode { get; set; }
-        public string Country { get; set; }
-        public string HomePhone { get; set; }
-        public string Extension { get; set; }
-        public byte[] Photo { get; set; }
-        public string Notes { get; set; }
-        public int? ReportsTo { get; set; }
-        public string PhotoPath { get; set; }
-        [ForeignKey("ReportsTo")]
-        public virtual Employee Manager { get; set; }
-        public virtual ICollection<Order> Orders { get; set; }
-    }
+
     [Table("Order Details")]
     public class OrderDetail : IValidatableObject
     {
+        [Key]
+        public int OrderDetailsID { get; set; }
         //[Key, Column(Order = 1)]
         public int OrderID { get; set; }
         //[Key, Column(Order = 2)]
@@ -85,8 +48,6 @@ namespace HotTowelNorthwind.Models
         public decimal UnitPrice { get; set; }
         public short Quantity { get; set; }
         public float Discount { get; set; }
-        [Key]
-        public int OrderDetailsID { get; set; }
         public Order Orders { get; set; }
         [ForeignKey("ProductID")]
         public virtual Product Product { get; set; }
@@ -102,6 +63,7 @@ namespace HotTowelNorthwind.Models
             }
         }
     }
+
     [Table("Orders")]
     public class Order
     {
@@ -120,11 +82,9 @@ namespace HotTowelNorthwind.Models
         public string ShipCountry { get; set; }
         [ForeignKey("CustomerID")]
         public virtual Customer Customer { get; set; }
-        [ForeignKey("EmployeeID")]
-        public virtual Employee Employee { get; set; }
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
-        //public Shipper Shipper { get; set; }
     }
+
     [Table("Products")]
     public class Product
     {
@@ -136,8 +96,5 @@ namespace HotTowelNorthwind.Models
         public short? UnitsOnOrder { get; set; }
         public short? ReorderLevel { get; set; }
         public bool Discontinued { get; set; }
-        // public Category Category  { get ;  set;}
-        public ICollection<OrderDetail> Order_Detail { get; set; }
-        // public Supplier Suppliers { get ;  set;}
     }
 }
